e9836f4e96741d3fdc8019aaa547df7b39f49624
Emelit0 <stollk811@gmail.com>
2022-12-13 20:23:26 +0100

    add react persist configs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import authReducer from ".state";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import persistReducer from "redux-persist/es/persistReducer";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//redux persit config
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
