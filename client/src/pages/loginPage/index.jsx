6cd1fd12bcfe5e594d384c2b19ac439de8ddd082
Emelit0 <stollk811@gmail.com>
2022-12-13 19:52:37 +0100

    add pages with boilerplate

---
+++ client/src/pages/loginPage/index.jsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>HomePage</div>;
+};
+
+export default LoginPage;
---
+++ client/src/pages/navbar/index.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>HomePage</div>;
+};
+
+export default Navbar;
