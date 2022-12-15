92353558b79e972436f9c8b90a9a10f9064f386c
Emelit0 <stollk811@gmail.com>
2022-12-15 22:18:19 +0100

    start loginPage

--- client/src/pages/loginPage/index.jsx
+++ client/src/pages/loginPage/index.jsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Typographie,
+  useTheme,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+
 const LoginPage = () => {
-  return <div>HomePage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColo={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
