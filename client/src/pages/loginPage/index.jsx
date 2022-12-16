c99d4c0d797f7cbf0ada348350817f0b3f8300d7
Emelit0 <stollk811@gmail.com>
2022-12-16 23:47:33 +0100

    add loginSchema and registerSchema

--- client/src/pages/loginPage/index.jsx
+++ client/src/pages/loginPage/index.jsx
@@ -1,10 +1,5 @@
-import {
-  Box,
-  Typographie,
-  useTheme,
-  useMediaQuery,
-  Typography,
-} from "@mui/material";
+import { Box, useTheme, useMediaQuery, Typography } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
   const theme = useTheme();
@@ -21,6 +16,18 @@ const LoginPage = () => {
         <Typography fontWeight="bold" fontSize="32px" color="primary">
           Sociopedia
         </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50px" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgoundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socialpedia, the Soail media clone
+        </Typography>
+        <Form />
       </Box>
     </Box>
   );
