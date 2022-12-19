282a0f7b79c0240606af3eb7729c6f22a53116ae
Emelit0 <stollk811@gmail.com>
2022-12-19 21:13:25 +0100

    add UserWidget

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -1,6 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "../navbar/index";
+import UserWidget from "../widgets/UserWidget.jsx";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
@@ -15,7 +16,16 @@ const HomePage = () => {
         display={isNonMobileScreens ? "flex" : "block"}
         gap="0.5rem"
         justifyContent="space-between"
-      ></Box>
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
+      </Box>
     </Box>
   );
 };
