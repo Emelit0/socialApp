52b8f7cdc186f5ad66796f7bee6b894b04d2c792
Emelit0 <stollk811@gmail.com>
2022-12-20 22:01:55 +0100

    add addPost widget

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "../navbar/index";
 import UserWidget from "../widgets/UserWidget.jsx";
+import MyPostWidget from "../widgets/MyPostWidget.jsx";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
@@ -23,7 +24,9 @@ const HomePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
         {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
       </Box>
     </Box>
