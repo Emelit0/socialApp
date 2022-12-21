4c13199e147ee624462a019ed22a5c692ed39081
Emelit0 <stollk811@gmail.com>
2022-12-21 23:50:26 +0100

    setup dependencies for PostWidget

--- client/src/pages/widgets/PostWidget.jsx
+++ client/src/pages/widgets/PostWidget.jsx
@@ -5,3 +5,9 @@ import {
   ShareOutlined,
 } from "@mui/icons-material";
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween.jsx";
+import Friend from "../../components/Friend.jsx";
+import WidgetWrapper from "../../components/WidgetWrapper.jsx";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "../../state/index.js";
