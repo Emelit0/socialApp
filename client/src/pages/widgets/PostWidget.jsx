6a918c4685818bfbee6a5988a5bd2cf9741039fa
Emelit0 <stollk811@gmail.com>
2022-12-23 22:55:58 +0100

    work on PostWidget

--- client/src/pages/widgets/PostWidget.jsx
+++ client/src/pages/widgets/PostWidget.jsx
@@ -11,3 +11,28 @@ import WidgetWrapper from "../../components/WidgetWrapper.jsx";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "../../state/index.js";
+
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
+
+  const { palette } = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
+
+  return {};
+};
