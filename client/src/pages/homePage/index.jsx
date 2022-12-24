3fc2d34466bbadec1103a891263598c65964c56b
Emelit0 <stollk811@gmail.com>
2022-12-24 21:03:59 +0100

    finnish postwidget

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Navbar from "../navbar/index";
 import UserWidget from "../widgets/UserWidget.jsx";
 import MyPostWidget from "../widgets/MyPostWidget.jsx";
+import PostWidget from "pages/widgets/PostWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
@@ -26,6 +27,7 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
+          <PostWidget userId={_id} />
         </Box>
         {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
       </Box>
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -36,7 +36,7 @@ export const authSlice = createSlice({
     //only get updates post
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.posts_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
