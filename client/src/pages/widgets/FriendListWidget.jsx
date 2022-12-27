a7e84b56208803835b111f01bf810e52b42632b2
Emelit0 <stollk811@gmail.com>
2022-12-27 23:54:10 +0100

    create FriendListWidget

---
+++ client/src/pages/widgets/FriendListWidget.jsx
@@ -0,0 +1,44 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import Friend from "../../components/Friend.jsx";
+import WidgetWrapper from "../../components/WidgetWrapper.jsx";
+import { useEffect } from "react";
+import { useDispatch, useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+
+const FriendlistWidget = ({ userId }) => {
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default FriendlistWidget;
